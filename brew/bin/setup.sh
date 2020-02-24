dfcb9dc0f5cb06679a1ac226e797991668028446
altescy <altescy@fastmail.com>
2020-02-25 00:48:36 +0900

    modify profile

--- brew/bin/setup.sh
+++ brew/bin/setup.sh
@@ -4,6 +4,9 @@ case ${OSTYPE} in
   darwin*)
     ;;
   linux*)
-    echo 'eval $($HOME/.linuxbrew/bin/brew shellenv)' >> $HOME/.bash_profile
+    test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+    test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+    test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+    echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
     ;;
 esac
--- zsh/bin/setup.sh
+++ zsh/bin/setup.sh
@@ -1,4 +1,6 @@
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm -f $HOME/.zshrc
 ln -s `pwd`/zsh/conf.d/zshrc $HOME/.zshrc
-cat `pwd`/zsh/conf.d/profile >> $HOME/.bash_profile
+
+test -r ~/.bash_profile && cat `pwd`/zsh/conf.d/profile >> $HOME/.bash_profile
+cat `pwd`/zsh/conf.d/profile >> $HOME/.profile
