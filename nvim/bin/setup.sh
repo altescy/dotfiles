f4960723ac982dd14def624be1a068ed5b89534c
altescy <altescy@fastmail.com>
2020-02-24 19:51:49 +0900

    remove pip install

--- nvim/bin/setup.sh
+++ nvim/bin/setup.sh
@@ -5,6 +5,4 @@ ln -s `pwd`/nvim/conf.d/init.vim $HOME/.config/nvim/init.vim
 ln -s `pwd`/nvim/conf.d/dein.toml $HOME/.config/nvim/dein.toml
 ln -s `pwd`/nvim/conf.d/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
 
-pip install -U neovim
-
 nvim -u $HOME/.config/nvim/init.vim +qall
