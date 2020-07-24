03cb956899e9a1ba6165ecad98d27df33eabdcd6
altescy <altescy@fastmail.com>
2020-07-24 17:19:44 +0900

    use coc.nvim

--- nvim/bin/setup.sh
+++ nvim/bin/setup.sh
@@ -4,5 +4,6 @@ mkdir -p $HOME/.config/nvim
 ln -s `pwd`/nvim/conf.d/init.vim $HOME/.config/nvim/init.vim
 ln -s `pwd`/nvim/conf.d/dein.toml $HOME/.config/nvim/dein.toml
 ln -s `pwd`/nvim/conf.d/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
+ln -s `pwd`/nvim/conf.d/coc-settings.toml $HOME/.config/nvim/coc-settings.toml
 
 nvim -u $HOME/.config/nvim/init.vim +qall
