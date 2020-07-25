6771220bd7becd70fcd24c0721ad58e536cfb0d8
altescy <altescy@fastmail.com>
2020-07-26 07:02:35 +0900

    modify pyton check

--- bin/python_install.sh
+++ bin/python_install.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+if (type "anyenv" > /dev/null 2>&1); then
+    eval "$(anyenv init -)"
+fi
+
 if !(type "pyenv" > /dev/null 2>&1); then
-    if (type "anyenv" > /dev/null 2>&1); then
-        anyenv install pyenv
-        eval "$(anyenv init -)"
+    anyenv install pyenv
+    eval "$(anyenv init -)"
 
-        # Install latest conda
-        PYTHON_VERSION="miniconda3-latest"
-        pyenv install $PYTHON_VERSION
-        pyenv rehash
-        pyenv global $PYTHON_VERSION
-        eval "$(anyenv init -)"
+    # Install latest conda
+    PYTHON_VERSION="miniconda3-latest"
+    pyenv install $PYTHON_VERSION
+    pyenv rehash
+    pyenv global $PYTHON_VERSION
+    eval "$(anyenv init -)"
 
-        # Upgrade conda & python
-        conda update -y conda
-        conda install -y python=3.8
-    fi
+    # Upgrade conda & python
+    conda update -y conda
+    conda install -y python=3.8
 fi
 
 python --version
