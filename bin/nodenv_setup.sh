a0edd6413c5e167ca4ad8de7f87fef8a209bb224
altescy <altescy@fastmail.com>
2020-07-26 02:06:06 +0900

    fix node version

--- bin/nodenv_setup.sh
+++ bin/nodenv_setup.sh
@@ -4,7 +4,7 @@ anyenv install nodenv
 eval "$(anyenv init -)"
 
 # Install latest node
-NODE_VERSION=nodenv install -l | grep -E '^[0-9]+\.[0-9]+\.[0-9]$' | tail -n 1
+NODE_VERSION=`nodenv install -l | grep -E '^[0-9]+\.[0-9]+\.[0-9]$' | tail -n 1`
 yes | nodenv install $NODE_VERSION
 nodenv rehash
 nodenv global $NODE_VERSION
