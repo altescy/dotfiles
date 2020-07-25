337e9358bf498e8a7e1fecca5425865c238c84a6
altescy <altescy@fastmail.com>
2020-07-25 16:38:33 +0900

    add coc-package installation

--- nvim/bin/setup.sh
+++ nvim/bin/setup.sh
@@ -1,4 +1,5 @@
-rm -rf $HOME/.config/nvim
+#!/bin/bash
+rm -rf $HOME/.config/nvim $HOME/.config/coc $HOME/.cache/dein
 mkdir -p $HOME/.config/nvim
 
 ln -s `pwd`/nvim/conf.d/init.vim $HOME/.config/nvim/init.vim
@@ -6,4 +7,11 @@ ln -s `pwd`/nvim/conf.d/dein.toml $HOME/.config/nvim/dein.toml
 ln -s `pwd`/nvim/conf.d/dein_lazy.toml $HOME/.config/nvim/dein_lazy.toml
 ln -s `pwd`/nvim/conf.d/coc-settings.toml $HOME/.config/nvim/coc-settings.toml
 
-nvim -u $HOME/.config/nvim/init.vim +qall
+nvim --headless -u $HOME/.config/nvim/init.vim +qall
+
+# Install coc.nvim extensions
+mkdir -p $HOME/.config/coc/extensions
+cp `pwd`/nvim/conf.d/coc-package.json $HOME/.config/coc/extensions/package.json
+pushd $HOME/.config/coc/extensions
+npm install --no-package-lock
+popd
