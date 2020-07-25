43d5483f1293dfee557ea0bbd683463e3bfd8dd9
altescy <altescy@fastmail.com>
2020-07-26 05:48:19 +0900

    fix zsh installation

--- zsh/bin/install.sh
+++ zsh/bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if !(type "zsh" > /dev/null 2>&1); then
-    if !(type "brew" > /dev/null 2>&1); then
+    if (type "brew" > /dev/null 2>&1); then
         brew install zsh
     fi
 fi
