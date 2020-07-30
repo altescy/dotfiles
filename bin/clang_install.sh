3112803111f5e31d8a7d3f82100ec2f53463e64d
altescy <altescy@fastmail.com>
2020-07-30 17:59:22 +0900

    configure update-alternatives

--- bin/clang_install.sh
+++ bin/clang_install.sh
@@ -7,7 +7,10 @@ if !(type "clang" > /dev/null 2>&1); then
       ;;
     linux*)
       if (type "apt-get" > /dev/null 2>&1); then
-        sudo apt-get install -y clang clang-tools
+        sudo apt-get install -y clang-10 clang-tools-10 clangd-10
+        sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 1000
+        sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 1000
+        sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-10 1000
       fi
       ;;
   esac
