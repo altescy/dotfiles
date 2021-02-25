6b00037ad0c5a334eaf464d6f0e93f937db5d769
altescy <altescy@fastmail.com>
2021-02-25 21:27:13 +0900

    install anyenv without brew

--- bin/anyenv_install.sh
+++ bin/anyenv_install.sh
@@ -3,6 +3,10 @@
 if !(type "anyenv" > /dev/null 2>&1); then
     if (type "brew" > /dev/null 2>&1); then
         brew install anyenv
+    else
+        git clone https://github.com/anyenv/anyenv ~/.anyenv
+        export PATH="$HOME/.anyenv/bin:$PATH"
+        echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.zshrc
     fi
 fi
 
