2c90341524de262083769659bc36034f1a028c29
altescy <altescy@fastmail.com>
2022-04-17 17:13:58 +0900

    set -y option

--- bin/nim_install.sh
+++ bin/nim_install.sh
@@ -5,7 +5,9 @@ set -e
 export CHOOSENIM_NO_ANALYTICS=1
 
 if !(type "choosenim" > /dev/null 2>&1); then
-    curl https://nim-lang.org/choosenim/init.sh -sSf | sh
+    curl https://nim-lang.org/choosenim/init.sh -sSf > choosenim_init.sh
+    sh choosenim_init.sh -y
+    rm choosenim_init.sh
     export PATH="$HOME/.nimble/bin:$PATH"
 fi
 
