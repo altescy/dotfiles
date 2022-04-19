d96f384d26cbc8e315565f7186c598091be2f0b4
altescy <altescy@fastmail.com>
2022-04-19 19:12:53 +0900

    add vala-language-server

--- bin/vala_install.sh
+++ bin/vala_install.sh
@@ -3,11 +3,11 @@
 if !(type "valac" > /dev/null 2>&1); then
   case ${OSTYPE} in
     darwin*)
-      brew install vala
+      brew install vala vala-language-server
       ;;
     linux*)
       if (type "apt-get" > /dev/null 2>&1); then
-        sudo apt-get install -y valac
+        sudo apt-get install -y valac vala-language-server
       fi
       ;;
   esac
