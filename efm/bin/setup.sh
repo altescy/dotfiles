ec88c7309e45ac3a6327dce2e755f99a7b7099ab
altescy <altescy@fastmail.com>
2022-04-28 01:42:59 +0900

    crete config dir

--- efm/bin/setup.sh
+++ efm/bin/setup.sh
@@ -2,4 +2,7 @@
 
 set -e
 
-ln -s $(pwd)/efm/conf.d/config.yaml $HOME/.config/efm-langserver/config.yaml
+config_dir="$HOME/.config/efm-langserver"
+
+mkdir -p $config_dir
+ln -s $(pwd)/efm/conf.d/config.yaml $config_dir/config.yaml
