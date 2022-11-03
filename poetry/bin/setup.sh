5c1971f836b79964d4a6548384e264969993489a
altescy <altescy@fastmail.com>
2022-11-03 15:37:43 +0900

    add configs for zsh completion

--- poetry/bin/setup.sh
+++ poetry/bin/setup.sh
@@ -12,3 +12,6 @@ esac
 mkdir -p "$POETRY_HOME"
 rm -f "$POETRY_HOME/config.toml"
 ln -s "$PWD/poetry/conf.d/config.toml" "$POETRY_HOME/config.toml"
+
+mkdir -p ~/.zfunc
+poetry completions zsh > ~/.zfunc/_poetry
