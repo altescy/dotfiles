ca2bb1159e7a68f3b58428751654fb0c93d424bf
altescy <altescy@fastmail.com>
2025-05-03 20:10:49 +0900

    add colorizer

--- nvim/conf.d/lua/plugins/preference.lua
+++ nvim/conf.d/lua/plugins/preference.lua
@@ -142,4 +142,16 @@ return {
       })
     end,
   },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({
+        "lua",
+        "css",
+        "html",
+        "javascript",
+        "typescript",
+      })
+    end,
+  },
 }
