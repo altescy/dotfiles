060ff91023fb558227da2d6a7de9043faac76365
altescy <altescy@fastmail.com>
2025-05-03 20:18:49 +0900

    add neo-tree

--- nvim/conf.d/lua/plugins/finder.lua
+++ nvim/conf.d/lua/plugins/finder.lua
@@ -1,13 +1,27 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require('telescope').setup()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<C-k>', builtin.live_grep, { desc = 'Telescope live grep' })
+      require("telescope").setup()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Telescope find files" })
+      vim.keymap.set("n", "<C-k>", builtin.live_grep, { desc = "Telescope live grep" })
     end,
-  }
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    lazy = false,
+    opts = {},
+    config = function()
+      vim.api.nvim_set_keymap("n", "<C-n>", ":Neotree toggle<CR>", { noremap = true })
+    end,
+  },
 }
