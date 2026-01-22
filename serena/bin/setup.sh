ca26fd9b67dd97fa569de795d8a863d00a36af5a
altescy <me@altescy.jp>
2026-01-23 02:00:45 +0900

    copy config

--- serena/bin/setup.sh
+++ serena/bin/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -rf ~/.serena/serena_config.yml
-ln -s $PWD/serena/conf.d/serena_config.yml ~/.serena/serena_config.yml
+cp $PWD/serena/conf.d/serena_config.yml ~/.serena/serena_config.yml
